37333bc21faeafd7dd2c7b3b02c6be8e6edf009a
Hasmire <lagman.jrgreggvictor@gmail.com>
2023-08-31 09:40:24 +0800

    Initial files

---
+++ src/types/types.ts
@@ -0,0 +1,32 @@
+// SAMPLE TYPES
+export type ErrorObject = {
+  email?: string[]
+  password?: string[]
+  password_confirmation?: string[]
+}
+
+export type ApiResponse<T> = {
+  success: boolean
+  data: T
+}
+
+export type PaginationData<T> = {
+  items: T[]
+  totalItems: number
+  totalPages: number
+  currentPage: number
+}
+
+export type UserDetails = {
+  id: number
+  name: string
+  email: string
+  role: UserRole
+}
+
+export type UserSettings = {
+  darkMode: boolean
+  notifications: boolean
+}
+
+export type UserRole = {}
