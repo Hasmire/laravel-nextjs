320bf0c538c1d28b05f8c97714b0455df950cfbd
Hasmire <lagman.jrgreggvictor@gmail.com>
2023-09-05 12:09:53 +0800

    fixes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,7 @@ function Dashboard() {
     <AppLayout
       header={
         <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          Dashboard test
+          Dashboard
         </h2>
       }>
       <Head>
