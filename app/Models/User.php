ba57121038882f991b5074b20dafe5796db7e1c7
Hasmire <lagman.jrgreggvictor@gmail.com>
2023-09-05 10:25:23 +0800

    Added breeze modifications for laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable {
     use HasApiTokens, HasFactory, Notifiable;
@@ -41,4 +43,22 @@ class User extends Authenticatable {
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * The attributes should append
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'must_verify_email',
+    ];
+
+    /**
+     * MustVerifyEmail attribute
+     *
+     * @return boolean
+     */
+    public function getMustVerifyEmailAttribute() {
+        return auth()->user() instanceof MustVerifyEmail;
+    }
 }
